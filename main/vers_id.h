210a2d80dea8a7a25a1343cf476bf835223c944b
zicodxx <>
2010-02-23 15:05:50 +0000

    Make Version-check for UDP also cover Micro-versions; Making sure versioning will act same way as conf.h for MacOS
    (Dummying out. Can't be bothered with this bullshit.)

--- main/vers_id.h
+++ main/vers_id.h
@@ -14,4 +14,8 @@
 #define D2X_IVER (atoi(D2XMAJOR)*10000+atoi(D2XMINOR)*100)
 #define DESCENT_VERSION "D2X-Rebirth v" VERSION
 
+#define D2XMAJORi 0
+#define D2XMINORi 0
+#define D2XMICROi 0
+
 #endif /* _VERS_ID */
