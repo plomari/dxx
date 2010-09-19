9fb8748e73ab56877cbb522ac222eb10e6783290
kreatordxx <>
2010-09-19 11:53:39 +0000

    Pass -1 instead of 255 as the colour to ogl_ubitmapm_cs when drawing savegame previews, to make sure a black rectangle isn't drawn instead

--- main/state.c
+++ main/state.c
@@ -169,7 +169,7 @@ int state_callback(newmenu *menu, d_event *event, grs_bitmap *sc_bmp[])
 			gr_set_current_canvas(temp_canv);
 			scale_bitmap(sc_bmp[citem-1], vertbuf, 0 );
 			gr_set_current_canvas( save_canv );
-			ogl_ubitmapm_cs((grd_curcanv->cv_bitmap.bm_w/2)-FSPACX(THUMBNAIL_W/2),items[0].y-FSPACY(3),THUMBNAIL_W*FSPACX(1),THUMBNAIL_H*FSPACY(1),&temp_canv->cv_bitmap,255,F1_0);
+			ogl_ubitmapm_cs((grd_curcanv->cv_bitmap.bm_w/2)-FSPACX(THUMBNAIL_W/2),items[0].y-FSPACY(3),THUMBNAIL_W*FSPACX(1),THUMBNAIL_H*FSPACY(1),&temp_canv->cv_bitmap,-1,F1_0);
 			gr_free_canvas(temp_canv);
 		}
 		
