5e5e2f581babe91267297bf3460a9cb4a8ffff08
Kp <kp@valhallalegends.com>
2013-02-21 00:20:26 +0000

    Remove RCS embedded string to synchronize code with D1X

--- main/player.c
+++ main/player.c
@@ -8,9 +8,6 @@
 #include "player.h"
 #include "cfile.h"
 
-#ifdef RCS
-static char rcsid[] = "$Id: player.c,v 1.1.1.1 2006/03/17 19:56:48 zicodxx Exp $";
-#endif
 
 /*
  * reads a player_ship structure from a CFILE
