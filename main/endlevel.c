122e10aa77a8957563af6718d38d109e24a0f2e1
zico <zico@dxx-rebirth.com>
2016-03-30 14:31:13 +0200

    Removed subtraction of lifeleft for external_explosion in do_endlevel_sequence as it is already happening in object_mode_one() making the explosion animation play too fast

--- main/endlevel.c
+++ main/endlevel.c
@@ -546,7 +546,6 @@ void do_endlevel_frame()
 
 	if (ext_expl_playing) {
 
-		external_explosion.lifeleft -= FrameTime;
 		do_explosion_sequence(&external_explosion);
 
 		if (external_explosion.lifeleft < ext_expl_halflife)
