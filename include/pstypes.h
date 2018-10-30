a088296277ef163f8decfdbcd0674a766c0d1432
wm4 <wm4@nowhere>
2018-10-30 19:43:18 +0100

    pstypes: cleanups
    Note that this may technically change the types used for the "legacy"
    xbyte/ushort/uint typedefs.

--- include/pstypes.h
+++ include/pstypes.h
@@ -22,40 +22,13 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define _TYPES_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-// define a dboolean
-typedef int dboolean;
+typedef int8_t sbyte;
+typedef uint8_t ubyte;
+typedef uint16_t ushort;
+typedef uint32_t uint;
 
-//define a signed byte
-typedef signed char sbyte;
-
-//define unsigned types;
-typedef unsigned char ubyte;
-#if defined(_WIN32) || defined(macintosh)
-typedef unsigned short ushort;
-typedef unsigned int uint;
-#endif
-
-#if defined(_WIN32) || defined(__sun__) // platforms missing (u_)int??_t
-# include <SDL/SDL_types.h>
-#elif defined(macintosh) // misses (u_)int??_t and does not like SDL_types.h
-# include <MacTypes.h>
- typedef SInt16 int16_t;
- typedef SInt32 int32_t;
- typedef SInt64 int64_t;
- typedef UInt16 u_int16_t;
- typedef UInt32 u_int32_t;
- typedef UInt64 u_int64_t;
-#endif // macintosh
-#if defined(_WIN32) || defined(__sun__) // platforms missing u_int??_t
- typedef Uint16 u_int16_t;
- typedef Uint32 u_int32_t;
- typedef Uint64 u_int64_t;
-#endif // defined(_WIN32) || defined(__sun__)
-
-#ifdef _MSC_VER
-# include <stdlib.h> // this is where min and max are defined
-#endif
 #ifndef min
 #define min(a,b) (((a)>(b))?(b):(a))
 #endif
@@ -79,23 +52,6 @@ typedef unsigned int uint;
 # define FNAME_MAX 32
 #endif
 
-#include <stdbool.h>
-#include <stddef.h>
-
-// the following stuff has nothing to do with types but needed everywhere,
-// and since this file is included everywhere, it's here.
-#if  defined(__i386__) || defined(__ia64__) || defined(WIN32) || \
-(defined(__alpha__) || defined(__alpha)) || \
-defined(__arm__) || defined(ARM) || \
-(defined(__mips__) && defined(__MIPSEL__)) || \
-defined(__SYMBIAN32__) || \
-defined(__x86_64__) || \
-defined(__LITTLE_ENDIAN__)	// from physfs_internal.h
-//# define WORDS_BIGENDIAN 0
-#else
-# define WORDS_BIGENDIAN 1
-#endif
-
 #ifdef __GNUC__
 # define __pack__ __attribute__((packed))
 #elif defined(_MSC_VER)
@@ -107,10 +63,6 @@ defined(__LITTLE_ENDIAN__)	// from physfs_internal.h
 # define __pack__
 #else
 # error d2x will not work without packed structures
-#endif
-
-#ifdef _MSC_VER
-# define inline __inline
 #endif
 
 #ifndef PACKAGE_STRING
