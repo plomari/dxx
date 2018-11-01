88b40307c9be19ff3931feda31439a97959d3fdc
Kp <kp@valhallalegends.com>
2018-11-01 02:41:30 +0000

    Pass correct object to check_effect_blowup
    `check_effect_blowup` should receive the `laser_info` of the weapon that
    caused the blast.  Previously, it was given the `laser_info` of the
    parent of that weapon.  The parent was not of type `OBJ_WEAPON`, so
    passing its `laser_info` is meaningless.
    
    Fixes: 9bd1ba7c4748a77d1e8dab0d898f79bedba22eac
    
    (wm4: hopefully equivalent. Also the commit ID Kp cites if the initial
    d2x commit (except a different hash - different svn import?), not sure
    what's the purpose of that. Removed RELEASE-NOTES.txt entry:
    +* Fixed original game bug that could cause a test for a trigger to
    produce the wrong result.
    )

--- main/wall.c
+++ main/wall.c
@@ -1553,7 +1553,10 @@ static void bng_process_segment(object *objp, fix damage, segment *segp, int dep
 				if (dist < damage/2) {
 					dist = find_connected_distance(&pnt, segp-Segments, &objp->pos, objp->segnum, MAX_BLAST_GLASS_DEPTH, WID_RENDPAST_FLAG);
 					if ((dist > 0) && (dist < damage/2))
-						check_effect_blowup(segp, sidenum, &pnt, &Objects[objp->ctype.laser_info.parent_num], 1);
+					{
+						Assert(objp->type == OBJ_WEAPON);
+						check_effect_blowup(segp, sidenum, &pnt, objp, 1);
+					}
 				}
 			}
 		}
