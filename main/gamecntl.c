76a922e263a395e9b46fa3de128e2fefa9a9a8fb
wm4 <wm4@nowhere>
2018-11-03 17:51:21 +0100

    Make ESC during deathroll abort deathroll immediately
    There's no reason to wait.
    
    There used to be a bug in dxx 0.55.1, that allowed "inappropriate"
    deathroll abort, with broken state after respawn (robot couldn't shoot
    at you). This doesn't happen here, either due my old fix, or because it
    doesn't trigger the issue.

--- main/gamecntl.c
+++ main/gamecntl.c
@@ -512,8 +512,7 @@ int HandleDeathInput(d_event *event)
 		if (Player_exploded && !key_isfunc(key) && key != KEY_PAUSE && key)
 			Death_sequence_aborted  = 1;		//Any key but func or modifier aborts
 		if (key == KEY_ESC)
-			if (ConsoleObject->flags & OF_EXPLODING)
-				Death_sequence_aborted = 1;
+			Death_sequence_aborted = 1;
 	}
 
 	if (Player_exploded && (event->type == EVENT_JOYSTICK_BUTTON_UP || event->type == EVENT_MOUSE_BUTTON_UP))
