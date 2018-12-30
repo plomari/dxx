dxx-rebirth/dxx-rebirth

removed weapon_rate_scale() which should be obsolete due to other optimizations in network code

main/laser.c
@@ -596,19 +596,6 @@ void do_omega_stuff(object *parent_objp, vms_vector *firing_pos, object *weapon_
 
 }
 
-/*
- * In effort to reduce weapon fire traffic in Multiplayer games artificially decrease the fire rate down to 100ms between shots.
- * This will work for all weapons, even if game is modded. Exception being Omega which is hardcoded.
- */
-float weapon_rate_scale(int wp_id)
-{
-	if ( !(Game_mode & GM_MULTI) )
-		return 1.0;
-	if ( Weapon_info[wp_id].fire_wait >= f0_1 || Weapon_info[wp_id].fire_wait <= 0 || wp_id == OMEGA_ID )
-		return 1.0;
-	return (f0_1/Weapon_info[wp_id].fire_wait);
-}
-
 // ---------------------------------------------------------------------------------
 // Initializes a laser after Fire is pressed
 //	Returns object number.
@@ -687,7 +674,7 @@ int Laser_create_new( vms_vector * direction, vms_vector * position, int segnum,
 	if (weapon_type == FLARE_ID)
 		obj->mtype.phys_info.flags |= PF_STICK;		//this obj sticks to walls
 
-	obj->shields = Weapon_info[obj->id].strength[Difficulty_level]*weapon_rate_scale(obj->id);
+	obj->shields = Weapon_info[obj->id].strength[Difficulty_level];
 
 	// Fill in laser-specific data
 
@@ -1358,7 +1345,7 @@ void Flare_create(object *obj)
 {
 	fix	energy_usage;
 
-	energy_usage = Weapon_info[FLARE_ID].energy_usage*weapon_rate_scale(FLARE_ID);
+	energy_usage = Weapon_info[FLARE_ID].energy_usage;
 
 	if (Difficulty_level < 2)
 		energy_usage = fixmul(energy_usage, i2f(Difficulty_level+2)/4);
@@ -1592,7 +1579,7 @@ int do_laser_firing_player(void)
 		return 0;
 
 	weapon_index = Primary_weapon_to_weapon_info[Primary_weapon];
-	energy_used = Weapon_info[weapon_index].energy_usage*weapon_rate_scale(weapon_index);
+	energy_used = Weapon_info[weapon_index].energy_usage;
 	if (Primary_weapon == OMEGA_INDEX)
 		energy_used = 0;	//	Omega consumes energy when recharging, not when firing.
 
@@ -1604,7 +1591,7 @@ int do_laser_firing_player(void)
 		if (Game_mode & GM_MULTI)
 			energy_used *= 2;
 
-	ammo_used = Weapon_info[weapon_index].ammo_usage*weapon_rate_scale(weapon_index);
+	ammo_used = Weapon_info[weapon_index].ammo_usage;
 
 	addval = 2*FrameTime;
 	if (addval > F1_0)
@@ -1625,7 +1612,7 @@ int do_laser_firing_player(void)
 			int	laser_level, flags;
 
 			if (!cheats.rapidfire)
-				Next_laser_fire_time += Weapon_info[weapon_index].fire_wait*weapon_rate_scale(weapon_index);
+				Next_laser_fire_time += Weapon_info[weapon_index].fire_wait;
 			else
 				Next_laser_fire_time += F1_0/25;
 
@@ -1910,7 +1897,7 @@ void create_smart_children(object *objp, int num_smart_children)
 #endif
 
 	if (objp->type == OBJ_WEAPON && objp->id == EARTHSHAKER_ID)
-		blast_nearby_glass(objp, Weapon_info[EARTHSHAKER_ID].strength[Difficulty_level]*weapon_rate_scale(EARTHSHAKER_ID));
+		blast_nearby_glass(objp, Weapon_info[EARTHSHAKER_ID].strength[Difficulty_level];
 
 	if (((objp->type == OBJ_WEAPON) && (Weapon_info[objp->id].children != -1)) || (objp->type == OBJ_ROBOT)) {
 		int i;
@@ -2023,7 +2010,7 @@ void do_missile_firing(int drop_bomb)
 
 	if (Guided_missile[Player_num] && Guided_missile[Player_num]->signature==Guided_missile_sig[Player_num]) {
 		release_guided_missile(Player_num);
-		Next_missile_fire_time = GameTime64 + Weapon_info[Secondary_weapon_to_weapon_info[weapon]].fire_wait*weapon_rate_scale(Secondary_weapon_to_weapon_info[weapon]);
+		Next_missile_fire_time = GameTime64 + Weapon_info[Secondary_weapon_to_weapon_info[weapon]].fire_wait;
 		return;
 	}
 
@@ -2036,7 +2023,7 @@ void do_missile_firing(int drop_bomb)
 		weapon_id = Secondary_weapon_to_weapon_info[weapon];
 
 		if (!cheats.rapidfire)
-			Next_missile_fire_time = GameTime64 + Weapon_info[weapon_id].fire_wait*weapon_rate_scale(weapon_id);
+			Next_missile_fire_time = GameTime64 + Weapon_info[weapon_id].fire_wait;
 		else
 			Next_missile_fire_time = GameTime64 + F1_0/25;
 

plomari/dxx

main/laser.c
@@ -599,19 +599,6 @@ void do_omega_stuff(object *parent_objp, vms_vector *firing_pos, object *weapon_
 
 }
 
-/*
- * In effort to reduce weapon fire traffic in Multiplayer games artificially decrease the fire rate down to 100ms between shots.
- * This will work for all weapons, even if game is modded. Exception being Omega which is hardcoded.
- */
-float weapon_rate_scale(int wp_id)
-{
-	if ( !(Game_mode & GM_MULTI) )
-		return 1.0;
-	if ( Weapon_info[wp_id].fire_wait >= f0_1 || Weapon_info[wp_id].fire_wait <= 0 || wp_id == OMEGA_ID )
-		return 1.0;
-	return (f0_1/Weapon_info[wp_id].fire_wait);
-}
-
 // ---------------------------------------------------------------------------------
 // Initializes a laser after Fire is pressed
 //	Returns object number.
@@ -690,7 +677,7 @@ int Laser_create_new( vms_vector * direction, vms_vector * position, int segnum,
 	if (weapon_type == FLARE_ID)
 		obj->mtype.phys_info.flags |= PF_STICK;		//this obj sticks to walls
 
-	obj->shields = Weapon_info[obj->id].strength[Difficulty_level]*weapon_rate_scale(obj->id);
+	obj->shields = Weapon_info[obj->id].strength[Difficulty_level];
 
 	// Fill in laser-specific data
 
@@ -1363,7 +1350,7 @@ void Flare_create(object *obj)
 {
 	fix	energy_usage;
 
-	energy_usage = Weapon_info[FLARE_ID].energy_usage*weapon_rate_scale(FLARE_ID);
+	energy_usage = Weapon_info[FLARE_ID].energy_usage;
 
 	if (Difficulty_level < 2)
 		energy_usage = fixmul(energy_usage, i2f(Difficulty_level+2)/4);
@@ -1597,7 +1584,7 @@ int do_laser_firing_player(void)
 		return 0;
 
 	weapon_index = Primary_weapon_to_weapon_info[Primary_weapon];
-	energy_used = Weapon_info[weapon_index].energy_usage*weapon_rate_scale(weapon_index);
+	energy_used = Weapon_info[weapon_index].energy_usage;
 	if (Primary_weapon == OMEGA_INDEX)
 		energy_used = 0;	//	Omega consumes energy when recharging, not when firing.
 
@@ -1609,7 +1596,7 @@ int do_laser_firing_player(void)
 		if (Game_mode & GM_MULTI)
 			energy_used *= 2;
 
-	ammo_used = Weapon_info[weapon_index].ammo_usage*weapon_rate_scale(weapon_index);
+	ammo_used = Weapon_info[weapon_index].ammo_usage;
 
 	addval = 2*FrameTime;
 	if (addval > F1_0)
@@ -1630,7 +1617,7 @@ int do_laser_firing_player(void)
 			int	laser_level, flags;
 
 			if (Laser_rapid_fire!=0xBADA55)
-				Next_laser_fire_time += Weapon_info[weapon_index].fire_wait*weapon_rate_scale(weapon_index);
+				Next_laser_fire_time += Weapon_info[weapon_index].fire_wait;
 			else
 				Next_laser_fire_time += F1_0/25;
 
@@ -1915,7 +1902,7 @@ void create_smart_children(object *objp, int num_smart_children)
 #endif
 
 	if (objp->type == OBJ_WEAPON && objp->id == EARTHSHAKER_ID)
-		blast_nearby_glass(objp, Weapon_info[EARTHSHAKER_ID].strength[Difficulty_level]*weapon_rate_scale(EARTHSHAKER_ID));
+		blast_nearby_glass(objp, Weapon_info[EARTHSHAKER_ID].strength[Difficulty_level]);
 
 	if (((objp->type == OBJ_WEAPON) && (Weapon_info[objp->id].children != -1)) || (objp->type == OBJ_ROBOT)) {
 		int i;
@@ -2028,8 +2015,7 @@ void do_missile_firing(int drop_bomb)
 
 	if (Guided_missile[Player_num] && Guided_missile[Player_num]->signature==Guided_missile_sig[Player_num]) {
 		release_guided_missile(Player_num);
-
-		Next_missile_fire_time = GameTime + Weapon_info[Secondary_weapon_to_weapon_info[weapon]].fire_wait*weapon_rate_scale(Secondary_weapon_to_weapon_info[weapon]);
+		Next_missile_fire_time = GameTime + Weapon_info[Secondary_weapon_to_weapon_info[weapon]].fire_wait;
 		return;
 	}
 
@@ -2042,7 +2028,7 @@ void do_missile_firing(int drop_bomb)
 		weapon_id = Secondary_weapon_to_weapon_info[weapon];
 
 		if (Laser_rapid_fire!=0xBADA55)
-			Next_missile_fire_time = GameTime + Weapon_info[weapon_id].fire_wait*weapon_rate_scale(weapon_id);
+			Next_missile_fire_time = GameTime + Weapon_info[weapon_id].fire_wait;
 		else
 			Next_missile_fire_time = GameTime + F1_0/25;
 