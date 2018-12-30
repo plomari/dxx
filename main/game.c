dxx-rebirth/dxx-rebirth

Make sure the Game_wind always gets freed when exiting the game screen, fixing flashing automap and framerate reduction after playing demos

main/game.c
@@ -1057,10 +1057,7 @@ int game_handler(window *wind, d_event *event, void *data)
 	}
 	
 	if (Function_mode != FMODE_GAME)
-	{
-		if (window_close(wind))
-			longjmp(LeaveGame,0);
-	}
+		longjmp(LeaveGame,0);
 
 	return 1;
 }
@@ -1080,6 +1077,7 @@ void game()
 			event_process();
 	}
 
+	window_close(wind);
 	digi_stop_all();
 
 	if ( (Newdemo_state == ND_STATE_RECORDING) || (Newdemo_state == ND_STATE_PAUSED) )

plomari/dxx

main/game.c
@@ -1215,10 +1215,7 @@ int game_handler(window *wind, d_event *event, void *data)
 	}
 	
 	if (Function_mode != FMODE_GAME)
-	{
-		if (window_close(wind))
-			longjmp(LeaveGame,0);
-	}
+		longjmp(LeaveGame,0);
 
 	return 1;
 }
@@ -1238,6 +1235,7 @@ void game()
 			event_process();
 	}
 
+	window_close(Game_wind);
 	digi_stop_all();
 
 	if ( (Newdemo_state == ND_STATE_RECORDING) || (Newdemo_state == ND_STATE_PAUSED) )