772319e00ebd482088112c561256db2937d90ae1
wm4 <wm4@nowhere>
2019-04-27 01:15:47 +0200

    render: update texmerge comments
    The old comment makes no sense. This isn't new code, but old unused
    code. It's only used if a certain debug option is enabled. Whatever.

--- main/render.c
+++ main/render.c
@@ -213,12 +213,13 @@ void render_face(int segnum, int sidenum, int nv, int *vp, int tmap1, int tmap2,
 			PIGGY_PAGE_IN(Textures[tmap2&0x3FFF]);
 			bm2 = &GameBitmaps[Textures[tmap2&0x3FFF].index];
 		}
+		// The GL renderer can't handle this specific case.
 		if (bm2 && (bm2->bm_flags&BM_FLAG_SUPER_TRANSPARENT)){
 			bm = texmerge_get_cached_bitmap( tmap1, tmap2 );
 			bm2 = NULL;
 		}
 	} else {
-		// New code for overlapping textures...
+		// Old code for overlapping textures (may be useful for debugging)
 		if (tmap2 != 0) {
 			bm = texmerge_get_cached_bitmap( tmap1, tmap2 );
 		} else {
