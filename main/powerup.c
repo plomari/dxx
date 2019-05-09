plomari/dxx

commit dd9950fb52c051fd174d7cdf991092c5dec50a8b

Add "saving" of invulnerability and cloaking
Invulnerability and cloaking are enabled by collecting powerups. D2X-XL
has the ability to collect these powerups without enabling them, and
then using them later. Not having this feature in "heavy" D2X-XL levels
is unfair (because these powerups may be located in places where it
makes no sense to use them), so I want it.

D2X-XL also seems to enable this by default for normal D2 levels. I
definitely don't want this. Normal D2 levels often place these powerups
in situations where you definitely want to enable them immediately, and
having to activate them separately would be confusing. And very
obnoxious, because the user first has to find out _how_ to do this
(while getting decimated by robots).

So make it configurable, and disable it by default. I'm using an
apparently yet unused key (F6) to show a dialog for this.

Pretty much everything involving these powerups is duplicated in the
original game code. I tried to avoid duplication in new code, but
there's still some. Don't blame me, blame the ones who wrote this over
20 years ago. Another strange thing is that the times (e.g. cloak_time)
are offset by the default duration. You can have other durations,
triggered by the "almighty" cheat, but also by this new feature if you
have collected more than 1 powerup. This didn't make too much sense in
the original game code, so it's, uh, confusing in the new code too.

When you get destroyed, saved up time will be emitted as powerups. Since
these powerups don't have state for storing the saved time, one powerup
is emitted for each quantum of default powerup time. If the rest time is
still half of the default time or more, a powerup is emitted, which
means the time the player will have when collecting them is rounded up.
I think this is fair. Normally, if you get destroyed while cloaked, you
get a new "full" powerup even if the remaining time was 1 second.

A small change by this commit is that I think the converter on flag was

main/powerup.c
@@ -447,8 +447,17 @@ int do_powerup(object *obj)
 			break;
 		case	POW_CLOAK:
 			if (Players[Player_num].flags & PLAYER_FLAGS_CLOAKED) {
-				HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "%s %s!",TXT_ALREADY_ARE,TXT_CLOAKED);
-				break;
+				if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) {
+					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved cloaking device!");
+					Players[Player_num].saved_cloak += CLOAK_TIME_MAX;
+					used = 1;
+				} else {
+					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "%s %s!",TXT_ALREADY_ARE,TXT_CLOAKED);
+				}
+			} else if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS_ALL) {
+				HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved cloaking device!");
+				Players[Player_num].saved_cloak += CLOAK_TIME_MAX;
+				used = 1;
 			} else {
 				Players[Player_num].cloak_time = (GameTime+CLOAK_TIME_MAX>i2f(0x7fff-600)?GameTime-i2f(0x7fff-600):GameTime);	//	Not! changed by awareness events (like player fires laser).
 				Players[Player_num].flags |= PLAYER_FLAGS_CLOAKED;
@@ -459,19 +468,28 @@ int do_powerup(object *obj)
 				#endif
 				powerup_basic(-10,-10,-10, CLOAK_SCORE, "%s!",TXT_CLOAKING_DEVICE);
 				used = 1;
-				break;
 			}
+			break;
 		case	POW_INVULNERABILITY:
 			if (Players[Player_num].flags & PLAYER_FLAGS_INVULNERABLE) {
-				HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "%s %s!",TXT_ALREADY_ARE,TXT_INVULNERABLE);
-				break;
+				if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) {
+					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved invulnerability!");
+					Players[Player_num].saved_invulnerable += INVULNERABLE_TIME_MAX;
+					used = 1;
+				} else {
+					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "%s %s!",TXT_ALREADY_ARE,TXT_INVULNERABLE);
+				}
+			} else if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS_ALL) {
+				HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved invulnerability!");
+				Players[Player_num].saved_invulnerable += INVULNERABLE_TIME_MAX;
+				used = 1;
 			} else {
 				Players[Player_num].invulnerable_time = (GameTime+INVULNERABLE_TIME_MAX>i2f(0x7fff-600)?GameTime-i2f(0x7fff-600):GameTime);
 				Players[Player_num].flags |= PLAYER_FLAGS_INVULNERABLE;
 				powerup_basic(7, 14, 21, INVULNERABILITY_SCORE, "%s!",TXT_INVULNERABILITY);
 				used = 1;
-				break;
 			}
+			break;
 		case	POW_MEGAWOW:
 			do_megawow_powerup(50);
 			used = 1;

main/collide.c
@@ -1856,6 +1856,18 @@ void drop_player_eggs(object *playerobj)
 		if (Players[pnum].flags & PLAYER_FLAGS_CLOAKED)
 			call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_CLOAK);
 
+		int drop_invuln =
+			(Players[pnum].saved_invulnerable + INVULNERABLE_TIME_MAX / 2) /
+			INVULNERABLE_TIME_MAX;
+		for (int n = 0; n < drop_invuln; n++)
+			call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_INVULNERABILITY);
+
+		int drop_cloak =
+			(Players[pnum].saved_cloak + CLOAK_TIME_MAX / 2) /
+			CLOAK_TIME_MAX;
+		for (int n = 0; n < drop_cloak; n++)
+			call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_CLOAK);
+
 		if (Players[pnum].flags & PLAYER_FLAGS_MAP_ALL)
 			call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_FULL_MAP);
 

main/game.c
@@ -676,7 +676,7 @@ void show_help()
 	int nitems = 0;
 	newmenu_item *m;
 
-	MALLOC(m, newmenu_item, 26);
+	MALLOC(m, newmenu_item, 27);
 	if (!m)
 		return;
 
@@ -689,6 +689,7 @@ void show_help()
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = "F3\t  SWITCH COCKPIT MODES";
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = TXT_HELP_F4;
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = TXT_HELP_F5;
+	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = "F6\t  other game stuff menu";
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = "ALT-F7\t  SWITCH HUD MODES";
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = TXT_HELP_PAUSE;
 	m[nitems].type = NM_TYPE_TEXT; m[nitems++].text = TXT_HELP_PRTSCN;

main/gamecntl.c
@@ -129,6 +129,7 @@ extern void DropSecondaryWeapon();
 extern void DropCurrentWeapon();
 
 void do_cheat_menu(void);
+static void do_special_menu(void);
 
 int HandleGameKey(int key);
 int HandleSystemKey(int key);
@@ -1012,6 +1013,10 @@ int HandleSystemKey(int key)
 			change_guidebot_name();
 			break;
 
+		case KEY_F6:
+			do_special_menu();
+			break;
+
 		case KEY_ALTED + KEY_SHIFTED + KEY_F10:
 			songs_pause_resume();
 			break;
@@ -1868,6 +1873,125 @@ void do_cheat_menu()
 		Players[Player_num].primary_ammo[VULCAN_INDEX] = mm[9].value;
 		init_gauges();
 	}
+}
+
+// Add saved cloak or invulnerability to currently active state.
+static void use_cloak_invuln(int use_ival, player *pl, int pl_flag)
+{
+	fix *saved, *time, tmax;
+	if (pl_flag == PLAYER_FLAGS_CLOAKED) {
+		saved = &pl->saved_cloak;
+		time = &pl->cloak_time;
+		tmax = CLOAK_TIME_MAX;
+	} else if (pl_flag == PLAYER_FLAGS_INVULNERABLE) {
+		saved = &pl->saved_invulnerable;
+		time = &pl->invulnerable_time;
+		tmax = INVULNERABLE_TIME_MAX;
+	} else {
+		assert(0);
+	}
+
+	fix use_val = i2f(use_ival);
+	use_val = min(use_val, *saved);
+	if (use_val <= 0)
+		return;
+
+	if (!(pl->flags & pl_flag)) {
+		pl->flags |= pl_flag;
+		*time = GameTime - tmax; // start out with 0
+	}
+
+	*saved -= use_val;
+	*time += use_val;
+}
+
+// Transfer currently active cloak or invulnerability to saved values.
+static void save_cloak_invuln(player *pl, int pl_flag)
+{
+	fix *saved, *time, tmax;
+	if (pl_flag == PLAYER_FLAGS_CLOAKED) {
+		saved = &pl->saved_cloak;
+		time = &pl->cloak_time;
+		tmax = CLOAK_TIME_MAX;
+	} else if (pl_flag == PLAYER_FLAGS_INVULNERABLE) {
+		saved = &pl->saved_invulnerable;
+		time = &pl->invulnerable_time;
+		tmax = INVULNERABLE_TIME_MAX;
+	} else {
+		assert(0);
+	}
+
+	if (!(pl->flags & pl_flag))
+		return;
+
+	fix amount = *time + tmax - GameTime;
+	if (amount > 0)
+		*saved += amount;
+
+	pl->flags &= ~pl_flag;
+}
+
+// Special other game stuff menu.
+static void do_special_menu(void)
+{
+	int mmn = 0;
+	newmenu_item mm[16];
+
+	player *pl = &Players[Player_num];
+
+	int invuln = f2i(pl->saved_invulnerable);
+	int cloak = f2i(pl->saved_cloak);
+
+	int use_cloak_opt = mmn;
+	nm_set_item_number(&mm[mmn++],
+					   tprintf(80, "Use cloak (max %d seconds)", cloak),
+					   min(30, cloak), 0, cloak);
+
+	int use_invuln_opt = mmn;
+	nm_set_item_number(&mm[mmn++],
+					   tprintf(80, "Use invuln. (max %d seconds)", invuln),
+					   min(30, invuln), 0, invuln);
+
+	int save_cloak_opt = mmn;
+	nm_set_item_menu(&mm[mmn++], "Save current cloak");
+	int save_invuln_opt = mmn;
+	nm_set_item_menu(&mm[mmn++], "Save current invuln.");
+
+	int auto_save = 0;
+	if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS_ALL) {
+		auto_save = 1;
+	} else if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) {
+		auto_save = 2;
+	}
+	int opt_auto_save = mmn;
+	nm_set_item_radio(&mm[mmn++], "Never auto save", auto_save == 0, 0);
+	nm_set_item_radio(&mm[mmn++], "Always auto save", auto_save == 1, 0);
+	nm_set_item_radio(&mm[mmn++], "Auto save if active", auto_save == 2, 0);
+
+	int exit_opt = mmn;
+	nm_set_item_menu(&mm[mmn++], "Cancel");
+
+	mmn = newmenu_do1("Game Stuff", NULL, mmn, mm, NULL, NULL, exit_opt);
+
+	if (mmn == use_cloak_opt) {
+		use_cloak_invuln(mm[use_cloak_opt].value, pl, PLAYER_FLAGS_CLOAKED);
+	} else if (mmn == use_invuln_opt) {
+		use_cloak_invuln(mm[use_invuln_opt].value, pl, PLAYER_FLAGS_INVULNERABLE);
+	} else if (mmn == save_cloak_opt) {
+		save_cloak_invuln(pl, PLAYER_FLAGS_CLOAKED);
+	} else if (mmn == save_invuln_opt) {
+		save_cloak_invuln(pl, PLAYER_FLAGS_INVULNERABLE);
+	} else if (mmn == exit_opt || mmn < 0) {
+		// Don't apply the options below.
+		return;
+	}
+
+	Players[Player_num].flags &=
+		~(PLAYER_FLAGS_SAVE_ORBS_ALL | PLAYER_FLAGS_SAVE_ORBS);
+	if (mm[opt_auto_save + 1].value)
+		Players[Player_num].flags |= PLAYER_FLAGS_SAVE_ORBS_ALL;
+	if (mm[opt_auto_save + 2].value)
+		Players[Player_num].flags |= PLAYER_FLAGS_SAVE_ORBS;
 }
 
 int ReadControls(d_event *event)

main/gameseq.c
@@ -407,7 +407,11 @@ void init_player_stats_new_ship()
 {
 	int	i;
 
-	int preserve_flags = 0;
+	int preserve_flags = PLAYER_FLAGS_SETTINGS |
+						 PLAYER_FLAGS_BLUE_KEY |
+						 PLAYER_FLAGS_RED_KEY |
+						 PLAYER_FLAGS_GOLD_KEY |
+						 PLAYER_FLAGS_MAP_ALL_CHEAT;
 	if (PlayerCfg.ExtendedAmmoRack)
 		preserve_flags |= PLAYER_FLAGS_AMMO_RACK;
 	int restore_flags = preserve_flags & Players[Player_num].flags;
@@ -444,21 +448,13 @@ void init_player_stats_new_ship()
 	Primary_weapon = 0;
 	Secondary_weapon = 0;
 
-	Players[Player_num].flags &= ~(	PLAYER_FLAGS_QUAD_LASERS |
-												PLAYER_FLAGS_AFTERBURNER |
-												PLAYER_FLAGS_CLOAKED |
-												PLAYER_FLAGS_INVULNERABLE |
-												PLAYER_FLAGS_MAP_ALL |
-												PLAYER_FLAGS_CONVERTER |
-												PLAYER_FLAGS_AMMO_RACK |
-												PLAYER_FLAGS_HEADLIGHT |
-												PLAYER_FLAGS_HEADLIGHT_ON |
-												PLAYER_FLAGS_FLAG);
-
-	Players[Player_num].flags |= restore_flags;
+	Players[Player_num].flags = restore_flags;
 
 	Players[Player_num].cloak_time = 0;
 	Players[Player_num].invulnerable_time = 0;
+
+	Players[Player_num].saved_cloak = 0;
+	Players[Player_num].saved_invulnerable = 0;
 
 	Player_is_dead = 0;		//player no longer dead
 

main/player.h
@@ -54,6 +54,14 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define PLAYER_FLAGS_HEADLIGHT      8192    // Player has headlight boost
 #define PLAYER_FLAGS_HEADLIGHT_ON   16384   // is headlight on or off?
 #define PLAYER_FLAGS_CONVERTER_ON	(1 << 15) // converter toggle on
+#define PLAYER_FLAGS_SAVE_ORBS		(1 << 16) // save cloak/invuln. instead of using them
+#define PLAYER_FLAGS_SAVE_ORBS_ALL	(1 << 17) // like above, but even when cloak/invuln. not active
+
+#define PLAYER_FLAGS_SETTINGS ( 	\
+	PLAYER_FLAGS_CONVERTER_ON |		\
+	PLAYER_FLAGS_SAVE_ORBS |		\
+	PLAYER_FLAGS_SAVE_ORBS_ALL |	\
+	0)
 
 #define AFTERBURNER_MAX_TIME    (F1_0*5)    // Max time afterburner can be on.
 #define CALLSIGN_LEN                8       // so can use as filename (was: 12)
@@ -103,6 +111,9 @@ typedef struct player {
 
 	fix     cloak_time;             // Time cloaked
 	fix     invulnerable_time;      // Time invulnerable
+
+	fix     saved_cloak;            // "Saved up" unused cloak time
+	fix     saved_invulnerable;     // ...equivalent
 
 	short   KillGoalCount;          // Num of players killed this level
 	short   net_killed_total;       // Number of times killed total

main/state.c
@@ -72,7 +72,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 extern void game_disable_cheats();
 
-#define STATE_VERSION 27
+#define STATE_VERSION 28
 #define STATE_COMPATIBLE_VERSION 20
 // 0 - Put DGSS (Descent Game State Save) id at tof.
 // 1 - Added Difficulty level save
@@ -101,6 +101,7 @@ extern void game_disable_cheats();
 //	   save D2X-XL trigger info
 // 27- Save D2X-XL equipment centers
 //	   add TLV extension records
+// 28- Save player.saved_* fields
 
 // Things to change on next incompatible savegame change:
 // - add a way to save/restore objects in a backward/forward compatible way
@@ -180,9 +181,14 @@ static void serdes_player(struct serdes *sd, player *pl)
 	sd_sbyte(sd, &pl->connected);
 	sd_int(sd, &pl->objnum);
 
-	// n_packets_got/n_packets_sent
-	sd_pad(sd, 4);
-	sd_pad(sd, 4);
+	if (sd->file_version < 28) {
+		// used to be n_packets_got/n_packets_sent
+		sd_pad(sd, 4);
+		sd_pad(sd, 4);
+	} else {
+		sd_fix(sd, &pl->saved_cloak);
+		sd_fix(sd, &pl->saved_invulnerable);
+	}
 
 	sd_uint(sd, &pl->flags);
 	sd_fix(sd, &pl->energy);
@@ -954,6 +960,7 @@ int state_restore_all(int in_game, int secret_restore, char *filename_override)
 									 PLAYER_FLAGS_GOLD_KEY;
 				if (PlayerCfg.ExtendedAmmoRack)
 					preserve_flags |= PLAYER_FLAGS_AMMO_RACK;
+				preserve_flags |= PLAYER_FLAGS_SETTINGS;
 				saved_player.flags |= Players[Player_num].flags & preserve_flags;
 			}
 		}

commit 0b1f4225ad85be46d0935a54e901cd0b39bd0fa0

powerup: fix "always saving" choice for cloak/invuln powerups
I decided that the PLAYER_FLAGS_SAVE_ORBS_ALL and PLAYER_FLAGS_SAVE_ORBS
flags should be exclusive at one point. But the former still implies the
functionality of the latter, which I then forgot to handle properly.

Fixes: dd9950fb52c

main/powerup.c
@@ -447,7 +447,9 @@ int do_powerup(object *obj)
 			break;
 		case	POW_CLOAK:
 			if (Players[Player_num].flags & PLAYER_FLAGS_CLOAKED) {
-				if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) {
+				if ((Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) ||
+					(Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS_ALL))
+				{
 					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved cloaking device!");
 					Players[Player_num].saved_cloak += CLOAK_TIME_MAX;
 					used = 1;
@@ -472,7 +474,9 @@ int do_powerup(object *obj)
 			break;
 		case	POW_INVULNERABILITY:
 			if (Players[Player_num].flags & PLAYER_FLAGS_INVULNERABLE) {
-				if (Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) {
+				if ((Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS) ||
+					(Players[Player_num].flags & PLAYER_FLAGS_SAVE_ORBS_ALL))
+				{
 					HUD_init_message(HM_DEFAULT|HM_REDUNDANT|HM_MAYDUPL, "Saved invulnerability!");
 					Players[Player_num].saved_invulnerable += INVULNERABLE_TIME_MAX;
 					used = 1;